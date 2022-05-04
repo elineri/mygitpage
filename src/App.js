66f01c71a934c68e335fb3b6f41e9c0b7bd578bf
elineri <elin.ericstam@gmail.com>
2022-05-04 11:01:31 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import * as React from "react"
+import {Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="privacy" element={<Privacy/>}/>
+
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+        <li>Home</li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Welcome to the homepage</h2>
+      <p>You will get more information later</p>
+    </main>
+    </>
+  );
+}
+
+function About() {
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+        <li><Link to="/.">Home</Link></li>
+        <li>About</li>
+        <li><Link to="/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Welcome to the about page</h2>
+      <p>Test text skrivs här</p>
+    </main>
+    <section></section>
+    <footer></footer>
+    </>
+  );
+}
+
+function Privacy() {
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li>Privacy</li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Privacy</h2>
+      <p>We are very private</p>
+    </main>
+    </>
   );
 }
 
